b4b5b8d31b07789551a4aea02cc66496c1a40c46
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-12-02 07:59:24 +0700

    feat: change name and condition from `useRootMotionWhileNotMoving` to `alwaysUseRootMotion`

--- Scripts/EntityMovementSystems/RigidBodyEntityMovement.cs
+++ Scripts/EntityMovementSystems/RigidBodyEntityMovement.cs
@@ -43,11 +43,12 @@ namespace MultiplayerARPG
         public Color groundCheckGizmosColor = Color.blue;
 
         [Header("Root Motion Settings")]
+        [FormerlySerializedAs("useRootMotionWhileNotMoving")]
+        public bool alwaysUseRootMotion;
         public bool useRootMotionForMovement;
         public bool useRootMotionForAirMovement;
         public bool useRootMotionForJump;
         public bool useRootMotionForFall;
-        public bool useRootMotionWhileNotMoving;
         public bool useRootMotionUnderWater;
 
         [Header("Networking Settings")]
@@ -160,11 +161,11 @@ namespace MultiplayerARPG
                 afterCrawlingPauseMovementDuration = afterCrawlingPauseMovementDuration,
                 underWaterThreshold = underWaterThreshold,
                 autoSwimToSurface = autoSwimToSurface,
+                alwaysUseRootMotion = alwaysUseRootMotion,
                 useRootMotionForMovement = useRootMotionForMovement,
                 useRootMotionForAirMovement = useRootMotionForAirMovement,
                 useRootMotionForJump = useRootMotionForJump,
                 useRootMotionForFall = useRootMotionForFall,
-                useRootMotionWhileNotMoving = useRootMotionWhileNotMoving,
                 useRootMotionUnderWater = useRootMotionUnderWater,
                 snapThreshold = snapThreshold,
             };
@@ -398,7 +399,7 @@ namespace MultiplayerARPG
                 entityMovement.useRootMotionForAirMovement = useRootMotionForAirMovement;
                 entityMovement.useRootMotionForJump = useRootMotionForJump;
                 entityMovement.useRootMotionForFall = useRootMotionForFall;
-                entityMovement.useRootMotionWhileNotMoving = useRootMotionWhileNotMoving;
+                entityMovement.alwaysUseRootMotion = alwaysUseRootMotion;
                 entityMovement.useRootMotionUnderWater = useRootMotionUnderWater;
 
                 entityMovement.snapThreshold = snapThreshold;
